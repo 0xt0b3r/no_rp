50f2098135bb2f72b51295e15ca76349919a9891
w00pi <33754111+w00pi@users.noreply.github.com>
2019-01-06 00:05:54 +0100

    Update server.lua

--- no_rp/server.lua
+++ no_rp/server.lua
@@ -1,5 +1,4 @@
 RegisterServerEvent("W00PI:DisplayShare")
 AddEventHandler("W00PI:DisplayShare", function(message)
-	-- TRIGGER BACK TO CLIENT
     TriggerClientEvent("W00PI:DisplayTrigger", -1, message, source)
 end)
