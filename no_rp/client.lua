c2b1a9e7a2995ed5aef925de401f53fb61bc1ebe
w00pi <33754111+w00pi@users.noreply.github.com>
2019-01-06 00:05:37 +0100

    Update client.lua

--- no_rp/client.lua
+++ no_rp/client.lua
@@ -1,66 +1,43 @@
------------------------------
---- */* SCRIPT CONFIG */* ---
------------------------------
--- */* MESSAGE TO SHOW */*
 local message = "Out Of Roleplay \n Do not roleplay with me"
--- */* METERS TO SHOW */*
 local meters = 10
--- */* DISPLAY IMPORTANT VAR */*
-local nbrDisplaying = 1
-
----------------------------
---- */* SCRIPT CMDS */* ---
----------------------------
-
--- */* COMMAND */*
+-----------------------------------
+--           Commands            --
+-----------------------------------
 RegisterCommand("togglerp", function(source, args, raw)
-    -- TRIGGER
     TriggerServerEvent("W00PI:DisplayShare", message)  
 end) 
 
------------------------------
---- */* SCRIPT EVENTS */* ---
------------------------------
-
--- */* CLIENT EVENT FOR TRIGGER */*
 RegisterNetEvent("W00PI:DisplayTrigger")
 AddEventHandler("W00PI:DisplayTrigger", function(message, source)
-	-- */* DEFAULT VAR FOR DISPLAY TOGGLE */*
     local displaying = true 
-	
-	-- */* DISPLAY FUNC TRIG */*
-    DisplayFunc(GetPlayerFromServerId(source), 1 + (1*0.14), message)
+    DisplayFunc(GetPlayerFromServerId(source), message)
 end)
-
---------------------------------
---- */* SCRIPT FUNCTIONS */* ---
---------------------------------
-
--- */* DISPLAY FUNCTION */*
-function DisplayFunc(mePlayer, offset, text)
-	-- BASIC VAR TOGGLE FOR 1 COMMAND
-	if displaying == true then
+--------------------------------------
+--           Functions              --
+--------------------------------------
+function DisplayFunc(mePlayer, text)
+    
+    if displaying == true then
 		displaying = false
 	else
 		displaying = true
 	end
-	--	LOOP
+
     Citizen.CreateThread(function()
-        nbrDisplaying = nbrDisplaying + 1
         while displaying == true do
             Wait(0)
             local coordsMe = GetEntityCoords(GetPlayerPed(mePlayer), false)
             local coords = GetEntityCoords(PlayerPedId(), false)
-            local dist = GetDistanceBetweenCoords(coordsMe['x'], coordsMe['y'], coordsMe['z'], coords['x'], coords['y'], coords['z'], true)
+            local dist = GetDistanceBetweenCoords(coordsMe.x, coordsMe.y, coordsMe.z, coords.x, coords.y, coords.z, true)
             if dist < meters then
-                DrawText3D(coordsMe['x'], coordsMe['y'], coordsMe['z']+offset, text)
+                if displaying then
+                    DrawText3D(coordsMe.x, coordsMe.y, coordsMe.z + 1.35, text)
+                end
             end
         end
-        nbrDisplaying = nbrDisplaying - 1
     end)
 end
 
--- */* WRITE 3D TEXT OVERHEAD TEMPLATE FUNCTION */*
 function DrawText3D(x, y, z, text)
     local onScreen, _x, _y = World3dToScreen2d(x, y, z)
     local p = GetGameplayCamCoords()
